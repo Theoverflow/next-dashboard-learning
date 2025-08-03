1717545b51bb04b5467b2ae9f145266601baddf0
Théo Marini <theomarini@macvie.local>
2025-08-03 20:48:27 +0200

    Chapter 11 - 2

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,24 @@
-export default function Page(){
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import Pagination from '@/app/ui/invoices/pagination';
+import { Suspense } from 'react';
+import { lusitana } from "@/app/ui/font";
+
+export default async function Page(){
     return (
-        <p>
-            Invoice Page
-        </p>
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search invoices..." />
+                <CreateInvoice />
+            </div>
+            <div className="mt-5 flex w-full justify-center">
+
+            </div>
+        </div>
     )
 }
